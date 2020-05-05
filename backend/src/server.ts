5021d234973e9a7216d239fe9acbaf480868d171
Rafael Pivatto <rafael.pivatto@gmail.com>
2020-05-05 20:18:50 -0300

    add static files route

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,12 +2,14 @@ import 'reflect-metadata';
 
 import express from 'express';
 import routes from './routes';
+import uploadCOnfig from './config/upload';
 
 import './database';
 
 const app = express();
 
 app.use(express.json());
+app.use('/files', express.static(uploadCOnfig.directory));
 app.use(routes);
 
 app.listen(3333, () => {
