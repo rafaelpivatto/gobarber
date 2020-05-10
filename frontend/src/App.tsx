3ce1e077b6d1a740bdaa4f1f658d6b133d4fe643
Rafael Pivatto <rafael.pivatto@gmail.com>
2020-05-09 23:01:55 -0300

    Add front-end app and create a login page

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import GlobalStyle from './styles/global';
+import SignIn from './pages/Signin';
+
+const App: React.FC = () => (
+  <>
+    <GlobalStyle />
+    <SignIn />
+  </>
+);
+
+export default App;
