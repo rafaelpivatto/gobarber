fdaa40a1e4828c9da7bc3ad1e66921e3cac6fb4a
Rafael Pivatto <rafael.pivatto@gmail.com>
2020-05-11 22:45:15 -0300

    Add validations to sign in

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 
 import GlobalStyle from './styles/global';
-import SignIn from './pages/Signin';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
 
 const App: React.FC = () => (
   <>
-    <GlobalStyle />
     <SignIn />
+    <GlobalStyle />
   </>
 );
 
