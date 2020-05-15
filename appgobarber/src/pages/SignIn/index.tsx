7c4e20dcd83fc0a01ce437477674baa200215285
Rafael Pivatto <rafael.pivatto@gmail.com>
2020-05-14 21:32:33 -0300

    Add mobile fonts and some styles in logon page

--- appgobarber/src/pages/SignIn/index.tsx
+++ appgobarber/src/pages/SignIn/index.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import { Image } from 'react-native';
+import { Container, Title } from './styles';
 
-import { Container } from './styles';
+import logoImg from '../../assets/logo.png';
 
 const SignIn: React.FC = () => {
-  return <Container />;
+  return (
+    <Container>
+      <Image source={logoImg} />
+
+      <Title>Faça seu logon</Title>
+    </Container>
+  );
 };
 
 export default SignIn;
