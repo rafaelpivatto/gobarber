d70f74d457f2b38e24bb52a6556017f7945b00ea
Rafael Pivatto <rafael.pivatto@gmail.com>
2020-05-16 11:59:59 -0300

    Add signIn and signUp form validations

--- appgobarber/src/components/Input/index.tsx
+++ appgobarber/src/components/Input/index.tsx
@@ -68,7 +68,7 @@ const Input: React.RefForwardingComponent<InputRef, InputProps> = (
   }, [fieldName, registerField]);
 
   return (
-    <Container isFocused={isFocused}>
+    <Container isFocused={isFocused} isErrored={!!error}>
       <Icon
         name={icon}
         size={20}
