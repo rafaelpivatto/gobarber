11db65f00587d94eea070382229b0a5cf208d076
Rafael Pivatto <rafael.pivatto@gmail.com>
2020-05-21 20:48:34 -0300

    add unit test for feature create user

---
+++ backend/src/modules/users/repositories/fakes/FakeUsersRepository.ts
@@ -0,0 +1,42 @@
+import { uuid } from 'uuidv4';
+
+import IUserRepository from '@modules/users/repositories/IUsersRepository';
+import ICreateUserDTO from '@modules/users/dtos/ICreateUserDTO';
+
+import User from '../../infra/typeorm/entities/User';
+
+class UsersRepository implements IUserRepository {
+  private users: User[] = [];
+
+  public async findById(id: string): Promise<User | undefined> {
+    const user = this.users.find(u => u.id === id);
+
+    return user;
+  }
+
+  public async findByEmail(email: string): Promise<User | undefined> {
+    const user = this.users.find(u => u.email === email);
+
+    return user;
+  }
+
+  public async create(userData: ICreateUserDTO): Promise<User> {
+    const user = new User();
+
+    Object.assign(user, { id: uuid() }, userData);
+
+    this.users.push(user);
+
+    return user;
+  }
+
+  public async save(user: User): Promise<User> {
+    const findIndex = this.users.findIndex(findUser => findUser.id === user.id);
+
+    this.users[findIndex] = user;
+
+    return user;
+  }
+}
+
+export default UsersRepository;
