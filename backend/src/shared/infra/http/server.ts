605a6b2a466a3df37cb00638b6c764f99f02ffa1
Rafael Pivatto <rafael.pivatto@gmail.com>
2020-05-25 18:25:45 -0300

    backend: add forgot password email template and template engine

--- backend/src/shared/infra/http/server.ts
+++ backend/src/shared/infra/http/server.ts
@@ -26,6 +26,9 @@ app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
     });
   }
 
+  // eslint-disable-next-line no-console
+  console.log(err);
+
   return response.status(500).json({
     status: 'error',
     message: 'Internal server error',
