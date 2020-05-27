2298f4f1eec2d73ae2404b340c12205f6ec8d944
Rafael Pivatto <rafael.pivatto@gmail.com>
2020-05-27 19:01:03 -0300

    backend: add feature to list providers

--- backend/src/modules/users/repositories/fakes/FakeUsersRepository.ts
+++ backend/src/modules/users/repositories/fakes/FakeUsersRepository.ts
@@ -2,6 +2,7 @@ import { uuid } from 'uuidv4';
 
 import IUserRepository from '@modules/users/repositories/IUsersRepository';
 import ICreateUserDTO from '@modules/users/dtos/ICreateUserDTO';
+import IFindAllProvidersDTO from '@modules/users/dtos/IFindAllProvidersDTO';
 
 import User from '../../infra/typeorm/entities/User';
 
@@ -18,6 +19,18 @@ class FakeUsersRepository implements IUserRepository {
     const user = this.users.find(u => u.email === email);
 
     return user;
+  }
+
+  public async findAllProviders({
+    except_user_id,
+  }: IFindAllProvidersDTO): Promise<User[]> {
+    let { users } = this;
+
+    if (except_user_id) {
+      users = this.users.filter(user => user.id !== except_user_id);
+    }
+
+    return users;
   }
 
   public async create(userData: ICreateUserDTO): Promise<User> {
