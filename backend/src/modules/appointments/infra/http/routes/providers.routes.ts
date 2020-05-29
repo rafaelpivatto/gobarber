f0e83ead74e4d500e68bb312cf2258179fda3bbd
Rafael Pivatto <rafael.pivatto@gmail.com>
2020-05-28 22:52:46 -0300

    backend: add routes to get hour and month availability for provider

--- backend/src/modules/appointments/infra/http/routes/providers.routes.ts
+++ backend/src/modules/appointments/infra/http/routes/providers.routes.ts
@@ -2,12 +2,26 @@ import { Router } from 'express';
 
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 import ProvidersController from '../controllers/ProvidersController';
+import ProviderMonthAvailabilityController from '../controllers/ProviderMonthAvailabilityController';
+import ProviderDayAvailabilityController from '../controllers/ProviderDayAvailabilityController';
 
 const providersRouter = Router();
 const providersController = new ProvidersController();
+const providerMonthAvailabilityController = new ProviderMonthAvailabilityController();
+const providerDayAvailabilityController = new ProviderDayAvailabilityController();
 
 providersRouter.use(ensureAuthenticated);
 
 providersRouter.get('/', providersController.index);
+
+providersRouter.get(
+  '/:provider_id/month-availabiliity',
+  providerMonthAvailabilityController.index,
+);
+
+providersRouter.get(
+  '/:provider_id/day-availabiliity',
+  providerDayAvailabilityController.index,
+);
 
 export default providersRouter;
