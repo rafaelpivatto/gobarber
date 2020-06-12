07dd2b236a5f5279007bd6744c496a5b79b35114
Rafael Pivatto <rafael.pivatto@gmail.com>
2020-06-11 22:09:24 -0300

    backend: add user load for find daily appointments from provider

--- backend/src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
+++ backend/src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
@@ -64,6 +64,7 @@ class AppointmentsRepository implements IAppointmentRepository {
             `to_char(${dateFieldName}, 'DD-MM-YYYY') = '${parsedDay}-${parsedMonth}-${year}'`,
         ),
       },
+      relations: ['user'],
     });
 
     return appointments;
