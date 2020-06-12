e98c010f0dc198db957ac5b7048b6f3b8a1c938f
Rafael Pivatto <rafael.pivatto@gmail.com>
2020-06-11 22:01:34 -0300

    backend: fix some features

--- backend/src/modules/appointments/infra/http/controllers/ProviderAppointmentsController.ts
+++ backend/src/modules/appointments/infra/http/controllers/ProviderAppointmentsController.ts
@@ -6,15 +6,15 @@ import ListProviderAppointmentsService from '@modules/appointments/services/List
 export default class ProviderAppointmentsController {
   public async index(request: Request, response: Response): Promise<Response> {
     const provider_id = request.user.id;
-    const { day, month, year } = request.body;
+    const { day, month, year } = request.query;
 
     const listAppointments = container.resolve(ListProviderAppointmentsService);
 
     const appointments = await listAppointments.execute({
       provider_id,
-      day,
-      month,
-      year,
+      day: Number(day),
+      month: Number(month),
+      year: Number(year),
     });
 
     return response.json(appointments);
