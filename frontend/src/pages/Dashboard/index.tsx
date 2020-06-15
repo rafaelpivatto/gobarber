2a4cb724124d331c20f5ff592652cf000e80f7c3
Rafael Pivatto <rafael.pivatto@gmail.com>
2020-06-15 20:47:06 -0300

    frontend: add styles in dashboard to show next appointment

--- frontend/src/pages/Dashboard/index.tsx
+++ frontend/src/pages/Dashboard/index.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
 
-import { FiPower } from 'react-icons/fi';
-import { Container, Header, HeaderContent, Profile } from './styles';
+import { FiPower, FiClock } from 'react-icons/fi';
+import {
+  Container,
+  Header,
+  HeaderContent,
+  Profile,
+  Content,
+  Schedule,
+  Calendar,
+  NextAppointment,
+} from './styles';
 
 import logoImg from '../../assets/logo.svg';
 import { useAuth } from '../../hooks/auth';
@@ -27,6 +36,34 @@ const Dashboard: React.FC = () => {
           </button>
         </HeaderContent>
       </Header>
+
+      <Content>
+        <Schedule>
+          <h1>Horários agendados</h1>
+          <p>
+            <span>Hoje</span>
+            <span>Dia 06</span>
+            <span>Segunda-feira</span>
+          </p>
+
+          <NextAppointment>
+            <strong>Atentimento a seguir</strong>
+            <div>
+              <img
+                src="https://avatars2.githubusercontent.com/u/4967150?s=460&u=0ac8f402ff34172d9b1c5b1b5ce34da0f1a637c5&v=4"
+                alt="Rafael Pivatto"
+              />
+
+              <strong>Rafael PIvatto</strong>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+            </div>
+          </NextAppointment>
+        </Schedule>
+        <Calendar />
+      </Content>
     </Container>
   );
 };
