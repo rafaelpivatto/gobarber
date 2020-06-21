32ccd443f3f91737aeb9f616e4c21108cc17dfd4
Rafael Pivatto <rafael.pivatto@gmail.com>
2020-06-20 21:04:27 -0300

    backend: adjust informations when save on cache providers list

--- backend/src/modules/appointments/services/ListProvidersService.ts
+++ backend/src/modules/appointments/services/ListProvidersService.ts
@@ -1,4 +1,5 @@
 import { injectable, inject } from 'tsyringe';
+import { classToClass } from 'class-transformer';
 
 import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
@@ -29,7 +30,10 @@ class ListProvidersService {
         except_user_id: user_id,
       });
 
-      await this.cachProvider.save(`providers-list:${user_id}`, users);
+      await this.cachProvider.save(
+        `providers-list:${user_id}`,
+        classToClass(users),
+      );
     }
 
     return users;
